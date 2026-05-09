7f6996864ef0c89b6fe85f1e95fe08af618bf168
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2026-05-09 15:30:58 +0100

    Simplify robots rules by removing referrer logic

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -1,20 +1,9 @@
 import type { MetadataRoute } from "next";
-import { headers } from "next/headers";
 import { seoPages } from "@/lib/seo-pages";
-import {
-	SOCIAL_CRAWLER_ROBOTS_USER_AGENTS,
-	SOCIAL_REFERRER_DOMAINS,
-} from "@/lib/social-crawlers";
+import { SOCIAL_CRAWLER_ROBOTS_USER_AGENTS } from "@/lib/social-crawlers";
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
 	const seoPageSlugs = Object.keys(seoPages);
-	const headersList = await headers();
-	const referrer =
-		headersList.get("x-referrer") || headersList.get("referer") || "";
-
-	const isAllowedReferrer = SOCIAL_REFERRER_DOMAINS.some((domain) =>
-		referrer.includes(domain),
-	);
 
 	const disallowPaths = [
 		"/dashboard",
@@ -24,11 +13,6 @@ export default async function robots(): Promise<MetadataRoute.Robots> {
 		"/record",
 		"/home",
 	];
-	const searchDisallowPaths = [...disallowPaths];
-
-	if (!isAllowedReferrer) {
-		searchDisallowPaths.push("/s/*");
-	}
 
 	return {
 		rules: [
@@ -39,13 +23,8 @@ export default async function robots(): Promise<MetadataRoute.Robots> {
 			})),
 			{
 				userAgent: "*",
-				allow: [
-					"/",
-					"/blog/",
-					...seoPageSlugs.map((slug) => `/${slug}`),
-					...(isAllowedReferrer ? ["/s/*"] : []),
-				],
-				disallow: searchDisallowPaths,
+				allow: ["/", "/blog/", ...seoPageSlugs.map((slug) => `/${slug}`)],
+				disallow: disallowPaths,
 			},
 		],
 		sitemap: "https://cap.so/sitemap.xml",
